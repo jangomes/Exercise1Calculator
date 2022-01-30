49bf975caff52e746ee8372e022b0abb67dc7937
Janaina Gomes da Silva <21944@student.dorset-college.ie>
2022-01-30 22:03:39 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise1Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator p = new Calculator();
+
+            Console.WriteLine("Welcome to the Calculator");
+
+            Console.WriteLine("\nPlease insert your first number: ");
+            p.N1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nPlease insert your second number: ");
+            p.N2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nWrite one number of your option \n\n1 - Sum \n2 - Subtract \n3 - Multiply \n4 - Division \n");
+            p.Option = Convert.ToInt32(Console.ReadLine());
+            p.cal();
+
+        }
+
+    }
+}
